f58fac797b40aa0fb56311f05e34505b7e8c4af2
Olivier RAZE <olivier.raze@gmail.com>
2021-12-01 13:51:00 +0100

    Add day1 part2 and optimize day1 part1

--- src/main/java/day1/Part1.java
+++ src/main/java/day1/Part1.java
@@ -14,14 +14,9 @@ public class Part1 {
             int lastMaxVal = Integer.valueOf(values.get(0));
             int increaseValuesCounter = 0;
             for (int i = 1; i < values.size(); i++) {
-                if (Integer.valueOf(values.get(i)) > lastMaxVal) {
-                    System.out.println(values.get(i) + " (increased)");
-                    lastMaxVal = Integer.valueOf(values.get(i));
+                if (Integer.valueOf(values.get(i)) > lastMaxVal)
                     increaseValuesCounter++;
-                } else if (Integer.valueOf(values.get(i)) < lastMaxVal) {
-                    System.out.println(values.get(i) + " (decreased)");
-                    lastMaxVal = Integer.valueOf(values.get(i));
-                }
+                lastMaxVal = Integer.valueOf(values.get(i));
             }
             System.out.println("The first response is : " + increaseValuesCounter);
         } catch (URISyntaxException | IOException e) {
